ba128467cd6f8a72bbb5797856f16c749711361a
binarybase <87497154+binarybase@users.noreply.github.com>
2022-10-07 16:59:01 +0200

    feat(js): subscription event emitter

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { RNScreenshotPrevent } = NativeModules;
+const eventEmitter = new NativeEventEmitter(RNScreenshotPrevent);
+
+/**
+ * subscribes to userDidTakeScreenshot event
+ * @param {function} callback handler
+ * @returns {function} unsubscribe fn
+ */
+export const addListener = (fn) => {
+    if(typeof(fn) !== 'function'){
+        console.error('RNScreenshotPrevent: addListener requires valid callback function');
+        return;
+    }
+
+    return eventEmitter.addListener("userDidTakeScreenshot", fn);
+}
 
 export default RNScreenshotPrevent;
